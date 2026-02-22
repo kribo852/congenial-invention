6bb28a892eaa6df0bb80736e9712ccb9762ecc95
kribo852 <kristoffer.a.borg@gmail.com>
2026-02-22 11:43:56 +0100

    Added colours to the weekdays
    Added PI day as a special day in the calendar

--- calendar/main.lua
+++ calendar/main.lua
@@ -12,7 +12,15 @@ function love.draw()
 	local animation_offset = animation_function()
 	local current_time_as_seconds = os.time(cur_date)
 	for i=0,120 do
-		love.graphics.printf(os.date("%a %d %b %Y", current_time_as_seconds+i*(24*3600)), 
+		if os.date("%a", current_time_as_seconds+i*(24*3600)) == "Sat" or 
+			os.date("%a", current_time_as_seconds+i*(24*3600)) == "Sun" then
+				love.graphics.setColor(0.3, 1.0, 0.4)
+		else
+				love.graphics.setColor(1.0, 0.6, 0.5)
+		end
+
+
+		love.graphics.printf(get_current_date_display(current_time_as_seconds, i), 
 			10+math.floor(i/30)*200, 19*(i%30)+animation_offset, width, "left")
 	end
 	love.graphics.print("Use up/down to navigate", 25, love.graphics.getHeight() - 15)
@@ -60,6 +68,17 @@ function next_day_animation()
 		end
 		return animation
 	end
+end
+
+function get_current_date_display(current_time_as_seconds, offset_in_days) 
+	
+	local day_and_month = os.date("%d%m", current_time_as_seconds+offset_in_days*(24*3600))
+
+	if day_and_month == "1403" then
+		return os.date("%a %d %b %Y", current_time_as_seconds+offset_in_days*(24*3600)).." PI day"
+	end
+
+	return os.date("%a %d %b %Y", current_time_as_seconds+offset_in_days*(24*3600))
 end
 
 
